51c53dba774f1313e880656bed64a97bbafcc931
mnorri <151025325+mnorri@users.noreply.github.com>
2024-04-08 17:22:24 +0900

    test

--- scripts.js
+++ scripts.js
@@ -16,7 +16,7 @@ const latestPosts = [
       link.addEventListener("click", function(event) {
         event.preventDefault();
         const postUrl = event.target.href;
-        console.error(postUrl);
+        console.event(postUrl);
         loadPostContent(postUrl);
       });
       listItem.appendChild(link);
